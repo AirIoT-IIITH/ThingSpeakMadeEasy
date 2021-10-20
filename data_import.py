14f6bbc2d73eba62f9ee2996bf74d3b6278f202b
spanddhana-sara <60307856+spanddhana-sara@users.noreply.github.com>
2021-10-20 19:12:31 +0530

    8000+ points
    Recursive method to download large no.of data points

--- data_import.py
+++ data_import.py
@@ -9,8 +9,22 @@ start_time = "2012:00:00"
 end_time = "2016:00:00"
 file_name= channel+start_date +".csv"
 
-URL = "https://api.thingspeak.com/channels/" + channel + "/feeds.csv?start=" +start_date +"%" +start_time + "&end=" +end_date +"%"+ end_time +"&timezone="+"Asia%2FKolkata"
+url = "https://api.thingspeak.com/channels/" + channel + "/feeds.csv?api_key=" +api_key +"&start=" +start_date +"%20" +start_time + "&end=" +end_date +"%20"+ end_time +"&timezone="+continent+"%2F"+city
+s=requests.get(url).content
+c=pd.read_csv(io.StringIO(s.decode('utf-8')))
+a = c
+x = c['created_at'].iloc[0].split(' ')[0]
+y = c['created_at'].iloc[0].split(' ')[1]
 
-print(URL)
 
-urllib.request.urlretrieve(URL, file_name)
+while len(a) == 8000:
+  if x != start_date and y != start_time :
+    end_date = x 
+    end_time = y
+    url = "https://api.thingspeak.com/channels/" + channel + "/feeds.csv?api_key=" +api_key +"&start=" +start_date +"%20" +start_time + "&end=" +end_date +"%20"+ end_time +"&timezone="+continent+"%2F"+city
+    s=requests.get(url).content
+    a = pd.read_csv(io.StringIO(s.decode('utf-8')))
+    c = a.merge(c, how = 'outer')
+    x = c['created_at'].iloc[0].split(' ')[0]
+    y = c['created_at'].iloc[0].split(' ')[1]
+c.to_csv(filename)
